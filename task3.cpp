c9d3775fd5cbb71e67bac54d04bb7f4cad88f4a9
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2021-10-31 21:36:06 +0530

    Update task3.cpp
    WAP to count the total number of calls for a member function from more than one objects. [Lets say, from 3 such Objects]

--- task3.cpp
+++ task3.cpp
@@ -1,32 +1,25 @@
 #include<iostream>
 using namespace std;
-class area
+class test
 {
-  int length,breadth,side;
- 
-  public:
-  area (int s);
-  area(int l,int b);
+	private:
+		static int count;
+		public:
+			test()
+			{
+				count++;
+			}
+			static int objcount()
+			{
+				return count;
+			}
 };
-
-area::area(int s)
-{
-  this->side=s;
-  cout<<"The area of square is:"<<(side*side)<<endl;
-}
-area::area(int l,int b)
-{
-  this->length=l;
-  this->breadth=b;
-  cout<<"The area of rectangle is:"<<(length*breadth)<<endl;
-}
+int test::count;
 int main()
 {
-  int s,l,b;
-  cout<<"Enter side of a square:";
-  cin>>s;
-  cout<<"Enter length and breadth of a rectangle:";
-  cin>>l>>b;
-  area a1=area(s);
-  area a2=area(l,b);
-  }
+	test obj1,obj2,obj3;
+	int cnt;
+	cnt=test::objcount();
+	cout<<"Number of objects:"<<cnt;
+	
+}
